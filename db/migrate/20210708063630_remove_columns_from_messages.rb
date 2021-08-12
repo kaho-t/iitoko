01db518f42a1e179e1c6c9802a66dce68d80d46d
kaho-t <tsuzyshoo@gmail.com>
2021-08-12 14:42:57 +0900

    retry message migration again

--- db/migrate/20210708063630_remove_columns_from_messages.rb
+++ db/migrate/20210708063630_remove_columns_from_messages.rb
@@ -1,7 +1,5 @@
 class RemoveColumnsFromMessages < ActiveRecord::Migration[6.1]
   def change
-    add_index :messages, [:talkroom_id, :sender_type, :created_at]
-    
     remove_index :messages, [:talkroom_id, :is_user, :created_at]
   end
 end
