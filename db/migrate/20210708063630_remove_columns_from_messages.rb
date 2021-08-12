7f01bd25a40e821556c7b597ffdeee39c236da6a
kaho-t <tsuzyshoo@gmail.com>
2021-08-12 14:39:10 +0900

    remove index 3839 to 3630

--- db/migrate/20210708063630_remove_columns_from_messages.rb
+++ db/migrate/20210708063630_remove_columns_from_messages.rb
@@ -1,5 +1,6 @@
 class RemoveColumnsFromMessages < ActiveRecord::Migration[6.1]
   def change
     remove_index :messages, [:talkroom_id, :is_user, :created_at]
+    add_index :messages, [:talkroom_id, :sender_type, :created_at]
   end
 end
