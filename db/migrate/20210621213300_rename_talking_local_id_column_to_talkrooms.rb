622fddfd8c0eede8a966ce04efb209ae509797d7
kaho-t <tsuzyshoo@gmail.com>
2021-08-12 14:24:19 +0900

    changed13300 again

--- db/migrate/20210621213300_rename_talking_local_id_column_to_talkrooms.rb
+++ db/migrate/20210621213300_rename_talking_local_id_column_to_talkrooms.rb
@@ -1,7 +1,5 @@
 class RenameTalkingLocalIdColumnToTalkrooms < ActiveRecord::Migration[6.1]
   def change
-    add_index :talkrooms, [:user_id, :local_id], unique: true
-
     remove_columns :talkrooms, :talking_user_id, :talking_local_id
   end
 end
